2223dabdff4e4712917a857321860fef55d857f7
rival <ri.valentini@reply.de>
2020-01-25 14:31:51 +0100

    Extend field definition: enums are now part of ::field spec

--- src/parser/xml_parser.clj
+++ src/parser/xml_parser.clj
@@ -4,21 +4,28 @@
 
 
 (spec/def ::some-type string?)
+(spec/def ::STRING string?)
 
 ;;TODO this is only an example -> generate the real thing with function below
-(def fields {:56 {:name :this-is-the-name :type :INT}
+(def fields {:56 {:name :this-is-the-name :type ::STRING :values {"SD" "SENIOR_SECURED", "SR" "SENIOR", "SB" "SUBORDINATED"}}
              :99 {:name :this-is-the-name :type :INT}
              :101 {:name :this-is-the-name :type ::some-type}})
 
+(defn key-value? [arg]
+  (and (coll? arg)
+       (= 2 (count arg))))
 
 (defn field? [arg]
   (let [[tag value] arg
-        field-attr (tag fields)]
+        field-attr (tag fields)
+        type (:type field-attr)]
     (if (some? field-attr)
-      (spec/valid? (:type field-attr) value))))
+      (if-let [enums (:values field-attr)]
+        (and (contains? enums value)
+             (spec/valid? type value))
+        (spec/valid? type value)))))
 
-(spec/def ::field field?)
-
+(spec/def ::field (spec/and key-value? field?))
 
 
 
@@ -35,13 +42,27 @@
 ;;TODO (2) Define possible field types as Spec
 
 
+(defn extract-enums [value-tags]
+  (let [result (map
+                 (fn [value-tag]
+                   {(get-in value-tag [:attrs :enum])
+                    (get-in value-tag [:attrs :description])})
+                 value-tags)]
+    (reduce merge {} result)))
+
+
 (defn build-field-map [fields]
-  (let [field-entries (map (fn [field]
-                      (let [name (get-in field [:attrs :name]) ;;TODO get possible INT values (=enum) from content for some fields
-                            type (keyword (get-in field [:attrs :type])) ;;TODO e.g. [{:tag :value, :attrs {:description NERC_EASTERN_OFF_PEAK, :enum 0}, :content nil}
-                            number (keyword (get-in field [:attrs :number]))] ;;TODO maybe solvable with map {:number [possible enums]} as spec
-                        {number {:name name :type type}}))
-                     fields)]
+  (let [field-entries (map
+                        (fn [field]
+                          (let [name (get-in field [:attrs :name]) ;;TODO get possible INT values (=enum) from content for some fields
+                                type (keyword (get-in field [:attrs :type])) ;;TODO e.g. [{:tag :value, :attrs {:description NERC_EASTERN_OFF_PEAK, :enum 0}, :content nil}
+                                number (keyword (get-in field [:attrs :number]))
+                                enums (extract-enums (get-in field [:content]))
+                                result {number {:name name :type type}}]
+                            (if (not-empty enums)
+                              (assoc-in result [number :values] enums)
+                              result)))
+                        fields)]
     (reduce merge {} field-entries)))
 
 
@@ -58,8 +79,12 @@
       (println field))
     (build-field-map fields)))
 
-(println
-  (spec/conform ::field [:101 "jndskncds"]))
+(println (spec/conform ::field [:56 "SD"]))
+(println (spec/conform ::field [:56 "GDFJ"]))
+(println (spec/conform ::field [:566 "SD"]))
+(println (spec/conform ::field 66))
+(println (spec/conform ::field [1 2 3]))
+(println (spec/conform ::field [:101 "jckjdsnds"]))
 
 (let [field-map (parse "resources/FIX50SP2.xml")]
   (println field-map)
