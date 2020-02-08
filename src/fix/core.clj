5ed7922033b2a27b0f7fbf0c28e2dc80a705589a
rival <ri.valentini@reply.de>
2020-02-08 20:23:56 +0100

    Add FIXT11.xml for missing header/trailer component and field definitions and describe a first data structure draft for repeating groups (core)

--- src/fix/core.clj
+++ src/fix/core.clj
@@ -14,8 +14,18 @@
 ;;TODO NEXT:
 ;;TODO (1) remove deprecated spec ns
 
+;;TODO (2) how to keep order and fast :tag access in groups? -> data within hashmap and separate vec for order
+;;TODO when the order is needed (e.g. Header/Trailer, repeating groups)
+;;TODO (3) how to handle repeating tags in hashmap? -> sub-maps repeating group as vec of maps
 
-
+(def message {:order [:345 :54 :9 :11]
+              :data {:345 "some_stuff"
+                     :54  "some_other_stuff"
+                     :9   "Fred"
+                     :10 3
+                     :11 [{:8 "A" :4 "B"} ;;TODO could be repeating group
+                          {:8 "C" :4 "D"}
+                          {:8 "E" :4 "F"}]}})
 
 
 
