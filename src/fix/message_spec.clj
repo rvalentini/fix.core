a1cf1a2fbe98a78b6165c4d12f5d9e52efdef6c7
rival <ri.valentini@reply.de>
2020-04-16 17:44:10 +0200

    Extend message validation implementation: + header extraction + header validation + header relevant field extraction

--- src/fix/message_spec.clj
+++ src/fix/message_spec.clj
@@ -1,9 +1,25 @@
 (ns fix.message-spec
-  (:require [clojure.spec.alpha :as spec]))
+  (:require [clojure.spec.alpha :as spec]
+            [fix.definitions.components :as c]
+            [fix.component-spec :as s]))
 
+
+(defn- extract-header [seq]
+  (take-while #(% (:definition (:Header c/components))) seq))
 
 (defn- validate-header [seq]
-  ;TODO extract header and validate (via definition)
+  (let [header (extract-header seq)]
+    (if (empty? header)
+      (throw (IllegalArgumentException. "Cannot extract any header information from message!"))
+      (if (spec/valid? ::s/component [header :Header])
+        ;TODO extract all relevant fields for further processing e.g. msg-type
+        (let [relevant-tags (filter #((:tag %) #{ :ALL-RELEVANT-TAGS}) header)
+              relevant-tags-as-map (reduce #(assoc %1 (:tag %2) (:value %2)) {} relevant-tags)])
+        ;TODO use threading
+        )
+
+      )
+    )
   )
 
 
