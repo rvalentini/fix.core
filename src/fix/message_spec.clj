ecad8c3695bbd6cfcc35dedbb7dbdb2d90be9cd4
rival <ri.valentini@reply.de>
2020-04-21 17:23:48 +0200

    Extend message validation: trailer section is now properly evaluated through checksum calculation

--- src/fix/message_spec.clj
+++ src/fix/message_spec.clj
@@ -11,9 +11,6 @@
         [body tail] (split-with #(nil? ((:tag %) (:definition (:Trailer c/components)))) rest)]
       [head body tail]))
 
-(defn- extract-header [seq]
-  (split-with #((:tag %) (:definition (:Header c/components))) seq))
-
 (defn- get-message-name-by-type [type]
   (if-let [msg (some #(if (= (:type (second %)) type) %) (seq m/messages))]
     (first msg)
@@ -24,37 +21,51 @@
   (println "Actual size: " (reduce #(+ %1 (:size %2)) 0 (drop-last rest)))
   (= (reduce #(+ %1 (:size %2)) 0 (drop-last rest)) length-in-bytes))
 
-(defn- evaluate-header [seq] ;TODO destructuring into [header body trailer] should happen outside
+(defn- evaluate-header [head] ;TODO destructuring into [header body trailer] should happen outside
   "Extracts and validates the FIX <StandardHeader> component block.
    The following TAGs are extracted and used for validation:
    :8 - BeginString
    :9 - BodyLength
    :35 - MessageType
    "
-  (let [[header body] (extract-header seq)]
-    (if (empty? header)
-      (throw (IllegalArgumentException. "Cannot extract any header information from message!"))
-      (if (spec/valid? ::s/component [header :Header])
-        (let [relevant-tags (->> header
-                                       (filter #((:tag %) #{:8 :9 :35}))
-                                       (reduce #(assoc %1 (:tag %2) (:value %2)) {}))]
-          (println "relevant tags: " relevant-tags)
-          (and
-            (contains? supported-versions (:8 relevant-tags))
-            (valid-body-length? seq (:9 relevant-tags))
-            (if-let [msg-name (get-message-name-by-type (:35 relevant-tags))]
-              (do
-                (println "Validating messagetype: " msg-name " with body: " body)
-                msg-name))))
-        (println "Header if not valid!")))))
+  (if (empty? head)
+    (throw (IllegalArgumentException. "Cannot extract any information: header section is empty!"))
+    (if (spec/valid? ::s/component [head :Header])
+      (let [relevant-tags (->> head
+                               (filter #((:tag %) #{:8 :9 :35}))
+                               (reduce #(assoc %1 (:tag %2) (:value %2)) {}))]
+        (println "relevant tags: " relevant-tags)
+        (and
+          (contains? supported-versions (:8 relevant-tags))
+          (valid-body-length? seq (:9 relevant-tags))
+          (if-let [msg-name (get-message-name-by-type (:35 relevant-tags))]
+            (do
+              (println "Validating messagetype: " msg-name)
+              msg-name))))
+      (println "Header if not valid!"))))
 
 ;TODO TEST checksum calculation with 8=FIX.4.19=6135=A34=149=EXEC52=20121105-23:24:0656=BANZAI98=0108=3010=003
-(int \a)
+(+ (int \A) (+ (int \2)) (+ (int \3)) (+ (int \5)))
+
+(seq (str 33333))
 
 
-(defn- valid-trailer? [seq]
-  ;TODO extract trailer and validate (only grab last 3 tags?)
-  )
+
+(defn- to-bytes [a]
+  (reduce + (map int (if (keyword? a)
+                       (drop 1 (seq (str a)))
+                       (seq (str a))))))
+
+(to-bytes 222)
+(drop 1 (seq (str :111)))
+
+(defn valid-trailer? [seq]
+  (let [checksum (:value (last seq))                       ;TODO make sure the last one is :10
+        sum-bytes (reduce #(+ %1 (to-bytes (:tag %2)) (to-bytes (:value %2))) 0 (drop-last seq))
+        sum-delimiters (count (drop-last seq))
+        sum-equal-symbols (* (count (drop-last seq)) (int \=))]
+    (println "checksum: " checksum " and calc: " (mod (+ sum-bytes sum-delimiters sum-equal-symbols) 256))
+    (= checksum  (mod (+ sum-bytes sum-delimiters sum-equal-symbols) 256))))
 
 (defn- is-message? [seq]
 
@@ -64,7 +75,7 @@
 
     ;TODO use head only for header eval
 
-    (let [msg-name (evaluate-header seq)]
+    (if-let [msg-name (evaluate-header head)]
       msg-name
       )
 
--- test/fix/message_spec_test.clj
+++ test/fix/message_spec_test.clj
@@ -4,6 +4,47 @@
             [clojure.spec.alpha :as spec]))
 
 
+(deftest checksum-calculation-test
+  (testing "Calculation of the messages checksum is correct"
+    ;8=FIX.4.19=6135=A34=149=EXEC52=20121105-23:24:0656=BANZAI98=0108=3010=003
+    (is (m/valid-trailer? [
+                           {:tag 8 :value "FIX.4.1"}
+                           {:tag 9 :value 61}
+                           {:tag 35 :value "A"}
+                           {:tag 34 :value 1}
+                           {:tag 49 :value "EXEC"}
+                           {:tag 52 :value "20121105-23:24:06"}
+                           {:tag 56 :value "BANZAI"}
+                           {:tag 98 :value 0}
+                           {:tag 108 :value 30}
+                           {:tag 10 :value 003}]))
+
+    ;8=FIX.4.19=8235=334=949=EXEC52=20121105-23:25:2556=BANZAI45=758=Unsupported message type10=002
+    (is (m/valid-trailer? [
+                           {:tag 8 :value "FIX.4.1"}
+                           {:tag 9 :value 82}
+                           {:tag 35 :value 3}
+                           {:tag 34 :value 9}
+                           {:tag 49 :value "EXEC"}
+                           {:tag 52 :value "20121105-23:25:25"}
+                           {:tag 56 :value "BANZAI"}
+                           {:tag 45 :value 7}
+                           {:tag 58 :value "Unsupported message type"}
+                           {:tag 10 :value 002}]))
+
+    (is (not (m/valid-trailer? [
+                                {:tag 8 :value "FIX.4.1"}
+                                {:tag 9 :value 82}
+                                {:tag 35 :value 3}
+                                {:tag 34 :value 9}
+                                {:tag 49 :value "EXEC"}
+                                {:tag 52 :value "20121105-23:25:25"}
+                                {:tag 56 :value "BANZAI"}
+                                {:tag 45 :value 7}
+                                {:tag 58 :value "Unsupported message type"}
+                                {:tag 10 :value 111}])))
+    ))
+
 
 (deftest message-destructuring-test
   (testing "Message is correctly destructured in [head body tail]"
@@ -77,4 +118,4 @@
 
 
 
-(message-destructuring-test)
+(checksum-calculation-test)
