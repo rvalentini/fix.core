4c5cf3d90b1a53c71fb9217e1c16961f0ed2f99d
rival <ri.valentini@reply.de>
2020-05-05 17:22:40 +0200

    Fix project.clj and License

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject fix.core "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+(defproject org.clojars.rival/fix.core "1.0.0"
+  :description "Financial Information eXchange (FIX) protocol validator using clojure.spec"
+  :url "https://github.com/rvalentini/fix.core"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v20.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.taoensso/timbre "4.10.0"]]
   :main ^:skip-aot fix.core
   :target-path "target/%s"
